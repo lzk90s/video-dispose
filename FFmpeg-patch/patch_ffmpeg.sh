a90311ffe753891d8e0465f6123f8289d76c5a19
liuzhikun <lzk@hzgosun.com>
2018-09-27 16:50:51 +0800

    11

--- FFmpeg-patch/patch_ffmpeg.sh
+++ FFmpeg-patch/patch_ffmpeg.sh
@@ -12,12 +12,13 @@ cp -rf libavfilter/* ${FFMPEG_HOME}/libavfilter
 
 # patch 
 allfilters_c_file=${FFMPEG_HOME}/libavfilter/allfilters.c
-fftool_makefile=${FFMPEG_HOME}/fftools/Makefile
+libavfilter_makefile=${FFMPEG_HOME}/libavfilter/Makefile
+
 already_exist=`grep ff_vf_algo ${allfilters_c_file} | wc -w`
 if [ 0 -eq ${already_exist} ]; then
 	echo "Patch allfilters.c"
 	sed -i 's/extern AVFilter ff_af_abench;/extern AVFilter ff_af_abench;\nextern AVFilter ff_vf_algo;/g' ${allfilters_c_file}
-	sed -i 's/vsink_nullsink.o/vsink_nullsink.o\nOBJS-$(CONFIG_ALGO_FILTER) += vf_algo.o\n/g' 
+	sed -i 's/vsink_nullsink.o/vsink_nullsink.o\nOBJS-$(CONFIG_ALGO_FILTER) += vf_algo.o\n/g' ${libavfilter_makefile}
 else
 	echo "Alreay patch, skip"
 fi
