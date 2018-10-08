3ae310d96574807b82a79037f3a53192fcbc70f3
liuzhikun <lzk@hzgosun.com>
2018-10-08 11:00:23 +0800

    11

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-make -C algo/vendor/seemmo -j -B -f Makefile_server install
-make -C algo/vendor/seemmo -j -B -f Makefile_stub install
-make -C algo/vendor/seemmo -j -B -f Makefile_wrapper install
-make -C algo/stub -j -B install
-make -C vfilter -j -B install
+make -C algo/vendor/seemmo -j$(nproc) -B -f Makefile_server install
+make -C algo/vendor/seemmo -j$(nproc) -B -f Makefile_stub install
+make -C algo/vendor/seemmo -j$(nproc) -B -f Makefile_wrapper install
+make -C algo/stub -j$(nproc) -B install
+make -C vfilter -j$(nproc) -B install
 
 
