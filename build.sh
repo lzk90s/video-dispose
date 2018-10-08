a71c4119d879c27e16ca57cc608a600ee8df5eec
liuzhikun <lzk@hzgosun.com>
2018-10-08 10:57:34 +0800

    更新makefile & dockerfile

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-make -C algo/vendor/seemmo -j -B -f Makefile_server
-make -C algo/vendor/seemmo -j -B -f Makefile_stub
-make -C algo/vendor/seemmo -j -B -f Makefile_wrapper
-make -C algo/stub -j -B
-make -C vfilter -j -B
+make -C algo/vendor/seemmo -j -B -f Makefile_server install
+make -C algo/vendor/seemmo -j -B -f Makefile_stub install
+make -C algo/vendor/seemmo -j -B -f Makefile_wrapper install
+make -C algo/stub -j -B install
+make -C vfilter -j -B install
 
 
