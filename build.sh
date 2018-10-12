6bc913f96fedfa8806b7a2eb15f91618034c34ee
liuzhikun <lzk@hzgosun.com>
2018-10-12 17:10:08 +0800

    999999

--- build.sh
+++ build.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 
 make -C algo/vendor/seemmo -j$(nproc) -B -f Makefile_server install
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 make -C algo/vendor/seemmo -j$(nproc) -B -f Makefile_stub install
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 
 make -C algo/vendor/seemmo -j$(nproc) -B -f Makefile_wrapper install
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 
 make -C algo/vendor/gosun -j$(nproc) -B -f Makefile_stub install
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 
 make -C algo/stub -j$(nproc) -B install
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 
 make -C vfilter -j$(nproc) -B install
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 
 make -C vfilter install_font
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
 	exit -1
 fi
 
